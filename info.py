818f9c427ba3838ead901c5599aee9a4c80e28b8
arseny_zimov <arsenij.zimov@gmail.com>
2024-08-13 12:58:11 +0300

    New game

---
+++ info.py
@@ -0,0 +1,52 @@
+# x = 10
+# # переменнная
+#
+# name = "roberts" # 10, 10.1, "text", 'text'
+#
+# store_items = ["bread", "eggs", "milk", "meat", "lamb", "apples"]
+# # Список (List)
+#
+# print(len(store_items))
+#
+# l = [[1,2,3],
+#      [4,5,6],
+#      [7,8,9]]
+#
+# # обратиться к одному элементу списка [index] (0, 1, 2, 3, 4 ..)
+# print(store_items[3])
+#
+# # Словарь (Dictionary)  {ключ: значение} {key: value}
+# store_items_count = {"bread": 100, "eggs": 50, "milk": 200}
+#
+# print(store_items_count["milk"])
+#
+#
+# def itemsCount(x, y):
+#     return x + y
+#
+#
+# for x in range(10):
+#     print(x)
+#
+#
+#
+
+
+for i in range(10):
+    for j in range(20):
+        print(i, j)
+
+
+[[(j, i) for j in range(20)] for i in range(10)]
+
+
+import os
+
+
+
+
+
+
+
+
+
